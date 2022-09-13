f82e284a124c369e12e004eeefe97ed8a0931a26
Waseem <wahid.waseem@gmail.com>
2022-09-12 20:59:10 -0700

    initial commit app setup for client

---
+++ src/Container/Enlightenment/Enlightenment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Enlightenment.css'
+
+const Enlightenment = () => {
+    return (
+        <div>Enlightenment</div>
+    )
+}
+
+export default Enlightenment
---
+++ src/Container/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About/About';
+export { default as Booking } from './Booking/Booking';
+export { default as Enlightenment } from './Enlightenment/Enlightenment';
+export { default as Header } from './Header/Header';
+export { default as Navbar } from './Navbar/Navbar';
