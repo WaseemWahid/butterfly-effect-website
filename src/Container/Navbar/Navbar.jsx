b88bca5b3e17591298d11d43d20601f27b91b5d2
Waseem <wahid.waseem@gmail.com>
2022-09-13 20:15:21 -0700

    starting navbar

--- src/Container/Navbar/Navbar.jsx
+++ src/Container/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
-        <div>Navbar</div>
+        <div className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <img src="" alt="navbar-logo" />
+            </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'enlightenment', 'booking', 'testimonials', 'booking'].map((item) => (
+                    <li className='app__flex p-text' key={`link-${ item }`}>
+                        <div />
+                        <a href={`#${ item }`}>{ item }</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
