ae3bde1acd3b2ca07b3c10947638f8428cd8b3ec
Waseem <wahid.waseem@gmail.com>
2022-10-24 21:48:54 -0700

    start with about section

--- src/Container/About/About.jsx
+++ src/Container/About/About.jsx
@@ -3,9 +3,9 @@ import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <div>
-                <h1>ABout the owner</h1>
+        <div className='app__about'>
+            <div className='app__about-container'>
+                <h1 className='app__about-mission'>My mission is to bring you clarity, insight, happiness, wonder, love and light.</h1>
             </div>
         </div>
     )
