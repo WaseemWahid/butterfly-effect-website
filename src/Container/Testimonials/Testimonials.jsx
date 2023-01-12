c31b9699bfb6bf9890ffaf95701071c9a3657373
Waseem <wahid.waseem@gmail.com>
2023-01-11 23:17:27 -0800

    Rework

--- src/Container/Testimonials/Testimonials.jsx
+++ src/Container/Testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ const Testimonials = () => {
     return (
         <div className='app__testimonials'>
             <h1>Testimonials</h1>
-            <p>Testimonials will be added here </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos inventore blanditiis suscipit quasi asperiores. Quod libero quaerat dicta mollitia, nesciunt et quibusdam illum! Labore nesciunt a odit deserunt at necessitatibus recusandae repellendus?</p>
         </div>
     )
 }
